b0e9239125a243180651badb7ef10e3c7b6a2224
Matheus Alves Pereira <matt.alpe.dev@gmail.com>
2026-06-01 22:35:53 -0300

    feat: arrays and slices

---
+++ internal/arraysandslices/sum.go
@@ -0,0 +1,33 @@
+package arraysandslices
+
+func Sum(numbers []int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	sums := make([]int, len(numbersToSum))
+
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	sums := make([]int, len(numbersToSum))
+
+	for i, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums[i] = 0
+		} else {
+			sums[i] = Sum(numbers[1:])
+		}
+	}
+
+	return sums
+}
