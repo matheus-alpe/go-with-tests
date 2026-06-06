23dd841903c26c1cdaffe08624a51fe8878f049d
Matheus Alves Pereira <matt.alpe.dev@gmail.com>
2026-06-06 11:11:36 -0300

    feat: dependency injection

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
-	"go-with-tests/internal/pointersanderrors"
+	"go-with-tests/internal/dependencyinjection"
 	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
-	_, err := pointersanderrors.ReadConfig()
-	if err != nil {
-		log.Fatalln(err)
-	}
+	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		dependencyinjection.Greet(w, "word")
+		dependencyinjection.Greet(os.Stdout, "Eloise <<<\n")
+	})))
 }
