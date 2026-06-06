580bec6aa808434ad1885951e7b95390dc9dc82a
Matheus Alves Pereira <matt.alpe.dev@gmail.com>
2026-06-06 11:49:13 -0300

    feat: mocking part 1

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,10 @@
 package main
 
 import (
-	"go-with-tests/internal/dependencyinjection"
-	"log"
-	"net/http"
+	"go-with-tests/internal/mocking"
 	"os"
 )
 
 func main() {
-	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		dependencyinjection.Greet(w, "word")
-		dependencyinjection.Greet(os.Stdout, "Eloise <<<\n")
-	})))
+	mocking.Countdown(os.Stdout, &mocking.DefaultSleeper{})
 }
---
+++ internal/mocking/countdown.go
@@ -0,0 +1,30 @@
+package mocking
+
+import (
+	"fmt"
+	"io"
+	"time"
+)
+
+const (
+	finalWord      = "Go!"
+	countdownStart = 3
+)
+
+type Sleeper interface {
+	Sleep()
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func Countdown(w io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(w, i)
+		sleeper.Sleep()
+	}
+	fmt.Fprint(w, finalWord)
+}
